30297fd8ffcc6567398e191e2dd3a4b2bfb970c2
bisuMei <62523428+bisuMei@users.noreply.github.com>
2020-07-15 21:15:49 +0300

    Update 1.20.py

--- src/1.20.py
+++ src/1.20.py
@@ -1,3 +1,6 @@
+# From http://effbot.org/zone/element-index.htm
+import xml.etree.ElementTree as etree
+
 dinner_recipe = '''<html><body><table>
 <tr><th>amt</th><th>unit</th><th>item</th></tr>
 <tr><td>24</td><td>slices</td><td>baguette</td></tr>
@@ -5,9 +8,6 @@ dinner_recipe = '''<html><body><table>
 <tr><td>1</td><td>cup</td><td>tomatoes</td></tr>
 <tr><td>1</td><td>jar</td><td>pesto</td></tr>
 </table></body></html>'''
-
-# From http://effbot.org/zone/element-index.htm
-import xml.etree.ElementTree as etree
 
 tree = etree.fromstring(dinner_recipe)
 
