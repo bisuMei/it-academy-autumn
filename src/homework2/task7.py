9b1c33701003931f6952f05eabda9fbc3e6fb966
bisuMei <bisumeikp@gmail.com>
2020-07-28 17:47:31 +0300

    Recast some tasks

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -1,64 +1,4 @@
 """
-First word:
-You are given a string where you have to find its first word.
-When solving a task pay attention to the following points:
-    There can be dots and commas in a string.
-    A string can start with a letter or, for example, a dot or space.
-    A word can contain an apostrophe and it's a part of a word.
-    The whole text can be represented with one word and that's it.
-"""
-
-
-def first_word(string):
-    """First word
-
-    :param string
-    :return: string
-    """
-
-    new_str = ''
-    n1 = ''
-    for i in string:
-        if i.isalpha() or i.isspace() or i == '\'' or i == '.':
-            new_str += i
-        else:
-            continue
-    n1 = new_str.replace('.', ' ')
-    new_lst = n1.split()
-
-    return new_lst[0]
-
-
-"""
-Checkio:
-You are given a non-empty list of integers (X).
-For this task, you should return a list consisting
-of only the non-unique elements in this list.
-To do so you will need to remove all unique
-elements (elements which are contained in a given
-list only once). When solving this task, do not
-change the order of the list. Example: [1, 2, 3, 1, 3]
-1 and 3 non-unique elements
-and result will be [1, 3, 1, 3].
-"""
-
-
-def checkio(list):
-    """Checkio
-
-    :param list
-    :return: An iterable of integers.
-    """
-
-    new_list = []
-    for i in list:
-        if list.count(i) > 1:
-            new_list.append(i)
-
-    return new_list
-
-
-"""
 Popular words:
 In this mission your task is to determine the popularity
 of certain words in the text. At the input of your function
@@ -92,6 +32,8 @@ def popular_words(string, list):
         my_dict.update({x: split_list.count(x)})
     return my_dict
 
+
+# Example:
 # print(popular_words('''
 # When I was One
 # I had just begun
@@ -116,11 +58,11 @@ def is_ascending(lst):
     """
 
     for i in range(1, len(lst)):
-        n = lst[-1 + i]
-        k = lst[i]
+        num_x = lst[-1 + i]
+        num_y = lst[i]
         if len(lst) < 2:
             break
-        elif n < k:
+        elif num_x < num_y:
             continue
         else:
             return False
@@ -156,3 +98,103 @@ def is_isogram(string):
         if value > 1:
             return False
     return True
+
+
+"""
+Write Morse decoder
+While the Morse code is now mostly superseded by voice and digital data communication channels,
+it still has its use in some applications around the world.
+The Morse code encodes every character as a sequence of "dots" and "dashes". For example,
+the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−.
+The Morse code is case-insensitive, traditionally capital letters are used.
+When the message is written in Morse code, a single space is used to separate the character
+codes and 3 spaces are used to separate words.
+For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+NOTE:
+Extra spaces before or after the code have no meaning and should be ignored.
+In addition to letters, digits and some punctuation, there are some special service codes,
+the most notorious of those is the international distress signal SOS
+(that was first issued by Titanic), that is coded as ···−−−···.
+These special codes are treated as single special characters, and usually are transmitted
+as separate words.
+
+Your task is to implement a function that would take the morse code as input
+and return a decoded human-readable string.
+"""
+
+
+def decodeMorse(morse_code):
+    """Decoder
+
+    :param morse_code:
+    :return: human-readable string
+    """
+    morse_code_dict = {'A': '.-', 'B': '-...',
+                       'C': '-.-.', 'D': '-..', 'E': '.',
+                       'F': '..-.', 'G': '--.', 'H': '....',
+                       'I': '..', 'J': '.---', 'K': '-.-',
+                       'L': '.-..', 'M': '--', 'N': '-.',
+                       'O': '---', 'P': '.--.', 'Q': '--.-',
+                       'R': '.-.', 'S': '...', 'T': '-',
+                       'U': '..-', 'V': '...-', 'W': '.--',
+                       'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                       '1': '.----', '2': '..---', '3': '...--',
+                       '4': '....-', '5': '.....', '6': '-....',
+                       '7': '--...', '8': '---..', '9': '----.',
+                       '0': '-----', ', ': '--..--', '.': '.-.-.-',
+                       '?': '..--..', '/': '-..-.', '-': '-....-',
+                       '(': '-.--.', ')': '-.--.-', '!': '-.-.--',
+                       'SOS': '...---...'}
+
+    chars_for_word = []
+    words = []
+    split_by_words = morse_code.split('   ')
+    for i in range(len(split_by_words)):
+        morse_word = split_by_words[i].split()
+        for unit in morse_word:
+            if unit.isalnum():
+                chars_for_word.append(" " + unit + " ")
+            for char, morse in morse_code_dict.items():
+                if unit == morse:
+                    chars_for_word.append(char)
+        words.append("".join(chars_for_word))
+        chars_for_word.clear()
+    result = " ".join(words)
+    return result.strip()
+
+
+# Example:
+# print(decodeMorse('.... . -.--   .--- ..- -.. .'))
+
+
+"""
+Sun Angle:
+Your task is to find the angle of the sun above the horizon knowing the time of the day. 
+Input data: 
+the sun rises in the East at 6:00 AM, which corresponds to the angle of 0 degrees. 
+At 12:00 PM the sun reaches its zenith, which means that the angle equals 90 degrees. 
+6:00 PM is the time of the sunset so the angle is 180 degrees. 
+If the input will be the time of the night (before 6:00 AM or after 6:00 PM), 
+your function should return - "I don't see the sun!".
+"""
+
+
+def sun_angle(time):
+    """Sun Angle
+
+    :param time: The time of the day
+    :return: The angle of the sun, rounded to 2 decimal places.
+    """
+    time_lst = time.split(':')
+    one_hour_angle = 15
+    one_minute_angle = 0.25
+    if int(time_lst[0]) == 18 and int(time_lst[1]):
+        return "I don't see the sun!"
+    elif int(time_lst[0]) >= 6 and int(time_lst[0]) <= 18:
+        sun_angle = (int(time_lst[0]) - 6) * one_hour_angle + int(time_lst[1]) * one_minute_angle
+        return sun_angle
+    else:
+        return "I don't see the sun!"
+
+# Example:
+# print(sun_angle('12:15'))
