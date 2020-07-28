69d92e844ff6e3fb6bb94ef44b94270622991cde
bisuMei <bisumeikp@gmail.com>
2020-07-28 17:56:48 +0300

    Task7 change

--- src/homework2/task7.py
+++ src/homework2/task7.py
@@ -102,21 +102,22 @@ def is_isogram(string):
 
 """
 Write Morse decoder
-While the Morse code is now mostly superseded by voice and digital data communication channels,
-it still has its use in some applications around the world.
-The Morse code encodes every character as a sequence of "dots" and "dashes". For example,
-the letter A is coded as ·−, letter Q is coded as −−·−, and digit 1 is coded as ·−−−−.
-The Morse code is case-insensitive, traditionally capital letters are used.
-When the message is written in Morse code, a single space is used to separate the character
-codes and 3 spaces are used to separate words.
-For example, the message HEY JUDE in Morse code is ···· · −·−−   ·−−− ··− −·· ·.
+While the Morse code is now mostly superseded by voice and digital data
+communication channels, it still has its use in some applications around
+the world. The Morse code encodes every character as a sequence of "dots"
+and "dashes". For example, the letter A is coded as ·−, letter Q is coded
+as −−·−, and digit 1 is coded as ·−−−−. The Morse code is case-insensitive,
+traditionally capital letters are used. When the message is written in Morse
+code, a single space is used to separate the character codes and 3 spaces are
+used to separate words. For example, the message HEY JUDE in Morse code is
+···· · −·−−   ·−−− ··− −·· ·.
 NOTE:
 Extra spaces before or after the code have no meaning and should be ignored.
-In addition to letters, digits and some punctuation, there are some special service codes,
-the most notorious of those is the international distress signal SOS
-(that was first issued by Titanic), that is coded as ···−−−···.
-These special codes are treated as single special characters, and usually are transmitted
-as separate words.
+In addition to letters, digits and some punctuation, there are some special
+service codes, the most notorious of those is the international distress signal
+SOS(that was first issued by Titanic), that is coded as ···−−−···.
+These special codes are treated as single special characters, and usually are
+transmitted as separate words.
 
 Your task is to implement a function that would take the morse code as input
 and return a decoded human-readable string.
@@ -169,12 +170,15 @@ def decodeMorse(morse_code):
 
 """
 Sun Angle:
-Your task is to find the angle of the sun above the horizon knowing the time of the day. 
-Input data: 
-the sun rises in the East at 6:00 AM, which corresponds to the angle of 0 degrees. 
-At 12:00 PM the sun reaches its zenith, which means that the angle equals 90 degrees. 
-6:00 PM is the time of the sunset so the angle is 180 degrees. 
-If the input will be the time of the night (before 6:00 AM or after 6:00 PM), 
+Your task is to find the angle of the sun above the horizon knowing the time 
+of the day.
+Input data:
+the sun rises in the East at 6:00 AM, which corresponds to the angle
+of 0 degrees.
+At 12:00 PM the sun reaches its zenith, which means that the angle 
+equals 90 degrees.
+6:00 PM is the time of the sunset so the angle is 180 degrees.
+If the input will be the time of the night (before 6:00 AM or after 6:00 PM),
 your function should return - "I don't see the sun!".
 """
 
