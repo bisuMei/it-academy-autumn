795a0b94a9a6cc1dceb4af0cdee6341439cd8a7d
bisuMei <62523428+bisuMei@users.noreply.github.com>
2020-08-03 18:01:13 +0300

    Update and rename 1.3.py to enumerate.py

--- src/enumerate.py
+++ src/enumerate.py
@@ -1,3 +1,8 @@
+"""
+Enumerate function
+"""
+
+
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print("iteration {iteration} is {name}".format(iteration=i, name=name))
